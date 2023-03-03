0ca5af75dfbef8f81ca5e20dc5a7446c6ab83507
Mariilauu <mmurillo90114@ufide.ac.cr>
2023-03-02 19:35:30 -0600

    Archivos generales del proyecto

---
+++ Código Proyecto/ProyectoG7.py
@@ -0,0 +1,23 @@
+#Fecha: 2/3/2023
+
+#Menú Principal
+#Presentará las opciones que se pueden realizar en el cajero.
+opcionMenuPrincipal = int(input("Bienvenido.\n Por favor digite el número de acuerdo a la opción que aparece en pantalla.\n1. Registrar nuevo usuario\n2. Usuario Registrado\n3. Configuración Avanzada\n4. Salir\nDigite su opción aquí: "))
+if opcionMenuPrincipal ==1:
+    #Registrar Usuario
+    #Permitirá almacenar el número de cédula del nuevo usuario.
+    cedulaNueva = int(input("Ingrese el número de cédula, por favor. NOTA: Esta no puede ser mayor o menor a 9 dígitos.\nCédula: "))
+    # Solicitud del Nombre.
+    # Solicitará el nombre del nuevo usuario.
+    nombreDeUsuario = input("Ingrese su nombre completo, por favor.\nNombre: ")
+    #Escogencia del PIN
+
+    #Depósito obligatorio
+
+    #Seleccionar automática y aleatoriamente tres servicios
+
+    #Guardar la información del nuevo usuario
+
+    #Regresar al menú
+
+
